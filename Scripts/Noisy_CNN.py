8e02b68e4534f0deffaca8c09f0e8e53e6c7ae6e
Nicolas  Tobis <nicolas.tobis18@imperial.ac.uk>
2019-07-05 12:35:34 +0100

    Changed imports

--- Scripts/Noisy_CNN.py
+++ Scripts/Noisy_CNN.py
@@ -1,6 +1,10 @@
 # Imports
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 import tensorflow as tf
 import numpy as np
 
