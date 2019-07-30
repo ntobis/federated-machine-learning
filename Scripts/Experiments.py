1cad0935d6c08653106b670bbaf46d2f749ec3a9
Nicolas  Tobis <nicolas.tobis18@imperial.ac.uk>
2019-07-30 19:05:56 +0100

    Changed Experiments

--- Scripts/Experiments.py
+++ Scripts/Experiments.py
@@ -566,7 +566,7 @@ def experiment_pain_centralized(dataset, experiment, rounds, shards=None, pretra
     else:
         # Initialize random model
         model = painCNN.build_cnn(test_data[0].shape)
-        model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+        model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
     # Load group 2 training data
     group_2_train_data, group_2_train_labels = dL.load_pain_data(group_2_train_path)
