fe13fc5829adc7e5cb144d2d34875f0cce52d3de
Nicolas  Tobis <nicolas.tobis18@imperial.ac.uk>
2019-08-10 16:37:53 +0100

    Updated MVP

--- Scripts/MVP.py
+++ Scripts/MVP.py
@@ -3,10 +3,9 @@ import sys
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 import keras
-
 from Scripts import Model_Architectures as mA
 from Scripts import Experiments
-
+from Scripts import Data_Loader_Functions as dL
 
 ROOT = os.path.dirname(os.path.dirname(__file__))
 DATA = os.path.join(ROOT, "Data", "Augmented Data", "Flexible Augmentation")
@@ -15,15 +14,22 @@ GROUP_2_PATH = os.path.join(DATA, "group_2")
 
 def main():
     model = mA.build_model((215, 215, 1), model_type='CNN')
-    early_stopping = keras.callbacks.EarlyStopping(monitor='val_loss', patience=3, verbose=1, mode='auto', baseline=None, restore_best_weights=True)
+    early_stopping = keras.callbacks.EarlyStopping(monitor='val_loss', patience=3, verbose=1, mode='auto',
+                                                   baseline=None, restore_best_weights=True)
     train_data, train_labels_binary = None, None
     for idx, folder in enumerate(sorted(os.listdir(GROUP_2_PATH))):
         f_path = os.path.join(GROUP_2_PATH, folder)
-        val_data, val_labels_binary, val_labels_people, val_labels = Experiments.load_and_prepare_data(f_path, 0, 4, 'CNN')
         if idx > 0:
+            val_data, val_labels_binary, val_labels_people, val_labels = Experiments.load_and_prepare_data(f_path, 0, 4,
+                                                                                                           'CNN')
             model.fit(train_data, train_labels_binary, batch_size=32, epochs=30,
                       validation_data=(val_data, val_labels_binary), callbacks=[early_stopping])
-        train_data, train_labels_binary = val_data, val_labels_binary
+
+        df = dL.create_pain_df(f_path)
+        print("{}: Actual number of images: ".format(folder), len(df), "thereof pain: ", sum(df['Pain'] != '0'))
+        df = dL.balance_data(df, threshold=200)
+        train_data, train_labels_binary, train_labels_people, train_labels = Experiments.load_and_prepare_data(
+            df['img_path'].values, 0, 4, 'CNN')
 
 
 if __name__ == '__main__':
