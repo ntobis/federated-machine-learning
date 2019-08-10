5707602514037f5744bb381935c8d421ef9fa1c9
Nicolas  Tobis <nicolas.tobis18@imperial.ac.uk>
2019-08-10 16:43:02 +0100

    Updated MVP

--- Scripts/MVP.py
+++ Scripts/MVP.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-import keras
+import tensorflow as tf
 from Scripts import Model_Architectures as mA
 from Scripts import Experiments
 from Scripts import Data_Loader_Functions as dL
@@ -14,10 +14,10 @@ GROUP_2_PATH = os.path.join(DATA, "group_2")
 
 def main():
     model = mA.build_model((215, 215, 1), model_type='CNN')
-    optimizer = keras.optimizers.RMSprop()
+    optimizer = tf.keras.optimizers.RMSprop()
     model.compile(optimizer, 'binary_crossentropy', ['accuracy'])
-    early_stopping = keras.callbacks.EarlyStopping(monitor='val_loss', patience=3, verbose=1, mode='auto',
-                                                   baseline=None, restore_best_weights=True)
+    early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=3, verbose=1, mode='auto',
+                                                      baseline=None, restore_best_weights=True)
     train_data, train_labels_binary = None, None
     for idx, folder in enumerate(sorted(os.listdir(GROUP_2_PATH))):
         f_path = os.path.join(GROUP_2_PATH, folder)
