a9d91a7eaba786baf7984f275af76e1df2d2d298
Nicolas  Tobis <nicolas.tobis18@imperial.ac.uk>
2019-08-29 17:22:10 +0100

    modified quick eval

--- Scripts/Experiments.py
+++ Scripts/Experiments.py
@@ -1152,11 +1152,11 @@ def quick_model_evaluation(f_path):
     df['paths'] = paths
     df['Shard'] = df['Shard'].apply(lambda x: x.split('-')[1].split('.')[0]).astype(int)
 
-    df_res = pd.DataFrame([file.split('_') for file in sorted(os.listdir(RESULTS))],
-                          columns=['Date', 'Pain', 'Experiment', 'Seed', 'TEST'])
+    df_res = pd.DataFrame([file.split('_') for file in sorted(os.listdir(RESULTS)) if '.csv' in file], columns=['Date', 'Pain', 'Experiment', 'Seed', 'TEST'])
+    df_res['Exp_Seed'] = df_res['Experiment'] + df_res['Seed']
     for seed, df_seed in df.groupby('Seed'):
         for experiment, df_experiment in df_seed.groupby('Experiment'):
-            if experiment in df_res['Experiment'] and seed in df_res['Seed']:
+            if experiment + seed in df_res['Exp_Seed']:
                 pass
             else:
                 print('Seed:', seed, 'Experiment:', experiment)
@@ -1169,8 +1169,8 @@ def quick_model_evaluation(f_path):
                                               metrics=metrics,
                                               f_path=f_path
                                               )
-            df_res = pd.DataFrame([file.split('_') for file in sorted(os.listdir(RESULTS))],
-                                  columns=['Date', 'Pain', 'Experiment', 'Seed', 'TEST'])
+            df_res = pd.DataFrame([file.split('_') for file in sorted(os.listdir(RESULTS)) if '.csv' in file], columns=['Date', 'Pain', 'Experiment', 'Seed', 'TEST'])
+            df_res['Exp_Seed'] = df_res['Experiment'] + df_res['Seed']
 
 
 def quick_baselines(f_path, learn_type):
