24dd2e925f1372bdb7c36e59e4b543c76b36f993
Nicolas  Tobis <nicolas.tobis18@imperial.ac.uk>
2019-09-02 17:27:11 +0100

    Focal Loss

--- Scripts/Keras_Custom.py
+++ Scripts/Keras_Custom.py
@@ -16,15 +16,7 @@ def focal_loss(gamma=2.0, alpha=0.25):
     return focal_loss_function
 
 
-def focal_loss_function(y_true, y_pred):
-    gamma = 2.0
-    alpha = 0.25
-    pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
-    pt_0 = tf.where(tf.equal(y_true, 0), y_pred, tf.zeros_like(y_pred))
-    return -K.sum(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1)) - K.sum(
-        (1 - alpha) * K.pow(pt_0, gamma) * K.log(1. - pt_0))
-
-class FocalLoss:
+class FocalLoss():
     def __init__(self, gamma=2.0, alpha=0.25):
         self.gamma = gamma
         self.alpha = alpha
