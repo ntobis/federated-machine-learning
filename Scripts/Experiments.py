5fc5daaf7f425d61cd128bc4baf99778bf5ebe76
Nicolas  Tobis <nicolas.tobis18@imperial.ac.uk>
2019-09-02 15:54:03 +0100

    Focal Loss

--- Scripts/Experiments.py
+++ Scripts/Experiments.py
@@ -26,7 +26,7 @@ from Scripts import Model_Architectures as mA
 from Scripts.Weights_Accountant import WeightsAccountant
 from Scripts.Keras_Custom import FocalLoss, focal_loss
 
-tf.keras.losses.custom_loss = focal_loss()
+tf.keras.losses.custom_loss = focal_loss
 
 # ------------------------------------------------------------------------------------------------------------------ #
 # ------------------------------------------------------ Paths ----------------------------------------------------- #
